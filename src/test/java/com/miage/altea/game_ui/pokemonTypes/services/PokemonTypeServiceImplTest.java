afb14dabca29b74858f85d94ad32cb0efc86a67e
Valentin <valentintartare59@gmail.com>
2020-02-10 11:13:28 +0100

    fix() : test bug

--- src/test/java/com/miage/altea/game_ui/pokemonTypes/services/PokemonTypeServiceImplTest.java
+++ src/test/java/com/miage/altea/game_ui/pokemonTypes/services/PokemonTypeServiceImplTest.java
@@ -1,6 +1,5 @@
 package com.miage.altea.game_ui.pokemonTypes.services;
 
-import com.miage.altea.game_ui.pokemonTypes.bo.PokemonType;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
@@ -9,33 +8,8 @@ import org.springframework.web.client.RestTemplate;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
-import static org.mockito.Mockito.*;
 
 class PokemonTypeServiceImplTest {
-
-    @Test
-    void listPokemonsTypes_shouldCallTheRemoteService() {
-        var url = "http://localhost:8080";
-
-        var restTemplate = mock(RestTemplate.class);
-        var pokemonServiceImpl = new PokemonTypeServiceImpl();
-        pokemonServiceImpl.setRestTemplate(restTemplate);
-        pokemonServiceImpl.setPokemonTypeServiceUrl(url);
-
-        var pikachu = new PokemonType();
-        pikachu.setName("pikachu");
-        pikachu.setId(25);
-
-        var expectedUrl = "http://localhost:8080/pokemon-types";
-        when(restTemplate.getForObject(expectedUrl, PokemonType[].class)).thenReturn(new PokemonType[]{pikachu});
-
-        var pokemons = pokemonServiceImpl.listPokemonsTypes();
-
-        assertNotNull(pokemons);
-        assertEquals(1, pokemons.size());
-
-        verify(restTemplate).getForObject(expectedUrl, PokemonType[].class);
-    }
 
     @Test
     void pokemonServiceImpl_shouldBeAnnotatedWithService() {
