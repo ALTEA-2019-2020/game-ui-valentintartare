725fce69741b2c1b73266b6f38a550e5d37d1e7a
Valentin <valentintartare59@gmail.com>
2020-02-10 10:57:30 +0100

    feat() : add language treatment

--- src/main/java/com/miage/altea/game_ui/pokemonTypes/services/PokemonTypeServiceImpl.java
+++ src/main/java/com/miage/altea/game_ui/pokemonTypes/services/PokemonTypeServiceImpl.java
@@ -4,10 +4,15 @@ import com.miage.altea.game_ui.pokemonTypes.bo.PokemonType;
 import lombok.NoArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.i18n.LocaleContextHolder;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 import java.util.Objects;
 
@@ -19,14 +24,22 @@ public class PokemonTypeServiceImpl implements PokemonTypeService {
 
     private String pokemonServiceUrl;
 
+
     public List<PokemonType> listPokemonsTypes() {
-        return Arrays.asList(Objects.requireNonNull
-                (restTemplate.getForObject(pokemonServiceUrl + "/pokemon-types/", PokemonType[].class)));
+        return Arrays.asList(Objects.requireNonNull(Objects.requireNonNull(
+                restTemplate.exchange(pokemonServiceUrl + "/pokemon-types/", HttpMethod.GET, this.getHttpEntity(), PokemonType[].class)).getBody())
+        );
     }
 
     public PokemonType pokemon(long id) {
         return Objects.requireNonNull
-                (restTemplate.getForObject(pokemonServiceUrl + "/pokemon-types/"+id, PokemonType.class));
+                (restTemplate.exchange(pokemonServiceUrl + "/pokemon-types/"+id, HttpMethod.GET, this.getHttpEntity(), PokemonType.class)).getBody();
+    }
+
+    private HttpEntity getHttpEntity(){
+        HttpHeaders headers = new HttpHeaders();
+        headers.setAcceptLanguageAsLocales(Collections.singletonList(LocaleContextHolder.getLocale()));
+        return new HttpEntity<>("body", headers);
     }
 
     @Autowired
